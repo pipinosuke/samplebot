ca9f2e87427e52eb04aedf9252ecd22be40690b4
pipinosuke <ys.pipinosuke@gmail.com>
2017-05-26 17:53:38 +0900

    dep

--- samplebot.rb
+++ samplebot.rb
@@ -5,6 +5,7 @@ require "dotenv"
 Dotenv.load
 TOKEN = ENV['SlackToken']
 
+
 Slack.configure {|config| config.token = TOKEN }
 client = Slack.realtime
 time = DateTime.now
@@ -18,24 +19,24 @@ client.on :hello do
   puts 'Successfully connected.'
 end
 
+  if time.hour == 07
+    Slackpost("おはよー","general")
+    Slackpost("<!channel>  そろそろ家賃払うぶー", "general") if date.day == 25
+  elsif time.hour == 17
+    Slackpost("今日もお疲れー","general")
+    Slackpost("<!channel> 明日は燃えるゴミの日","general") if date.wday == 6 || 3
+    Slackpost("<!channel> 明日は缶とペットボトルの日", "general" ) if date.wday == 4
+  end
+
 client.on :message do |data|
   # 得られたmessageにtestが含まれていて、botからのメッセージでなければ
   if data["subtype"] != "bot_message"
     Slackpost("うんこ！うんこ！", "general") if data["text"].include?("うん")
     Slackpost("おはよー", "general") if data["text"].include?("おは")
     Slackpost("おやすみ","general") if data["text"].include?("おやす")
-    Slackpost("#{time.hour}時#{time.min}分#{time.sec}秒です","general") if data["text"].include?("時間")
+    Slackpost("#{time.hour}時#{time.min}分#{time.sec}秒です","general") if data["text"].include?("時間")
   end
-
-    if time.hour == 07
-      Slackpost("おはよー", "general")
-      next unless date.day == 25
-      Slackpost("@channel そろそろ家賃払うぶー", "general")
-    elsif time.hour == 19
-      Slackpost("今日もお疲れー","general")
-      Slackpost("@channel 明日は燃えるゴミの日","general") if date.wday == 6 || 3
-      Slackpost("@channel 明日は缶とペットボトルの日", "general" ) if date.wday == 4
-    end
 end
+
 
 client.start
